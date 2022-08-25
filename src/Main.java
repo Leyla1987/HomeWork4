21f757541d57b858029795b9afee8b9e1972c341
Leyla1987 <glenmorangie@list.ru>
2022-08-25 21:46:22 +0300

    Выполнение ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //Задание 1
+        int i = 0;
+       while (i <= 10)  {
+            System.out.print(i  + " ");
+            i++;
+        }
+        System.out.print("\n");
+
+       for (; i >=0; i--) {
+           System.out.print(i  + " ");
+       }
+        System.out.print("\n");
+        //Задание 2
+
+
+       for (i = 4; i <= 31; i= i+7)
+       {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+        System.out.print("\n");
+        //Задание 3
+
+        for (i = 1896; i <= 2122; i= i+79) {
+            System.out.println(i);
+        }
     }
 }
